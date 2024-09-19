91e4b4faa55a6ea0c90874f88facb7dcf277db63
Salman Barick M <107459965+Salman-06@users.noreply.github.com>
2024-09-19 22:17:53 +0530

    Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -20,7 +20,6 @@ function Header() {
   const viewOpenSource = openSource.display;
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
-  const viewBlog = blogSection.display;
   const viewTalks = talkSection.display;
   const viewResume = resumeSection.display;
 
@@ -59,11 +58,6 @@ function Header() {
           {viewAchievement && (
             <li>
               <a href="#achievements">Achievements</a>
-            </li>
-          )}
-          {viewBlog && (
-            <li>
-              <a href="#blogs">Blogs</a>
             </li>
           )}
           {viewTalks && (
